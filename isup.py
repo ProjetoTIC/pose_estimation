cf7dbba169be86d17dc2fed0eaba584b6b7a5b04
julianopdf <141387859+julianopdf@users.noreply.github.com>
2024-01-08 17:10:33 -0300

    Add files via upload

--- isup.py
+++ isup.py
@@ -59,4 +59,30 @@ def isUpR(image, kpts, p1, p2, p3, draw=True):
             return True
             
 
+def Torso(image, kpts, p1, p2, p3, p4, draw=True):
+    coord = []
+    no_kpts = len(kpts)//3
+    for i in range(no_kpts):
+        cx, cy = kpts[3*i], kpts[3*i + 1]
+        conf = kpts[3*i + 2]
+        coord.append([i, cx,cy, conf])
+
+    points = (p1,p2,p3,p4)
+
+    x1,y1 = coord[p1][1:3]
+    x2,y2 = coord[p2][1:3]
+    x3,y3 = coord[p3][1:3]
+    x4,y4 = coord[p4][1:3]
+    
+
+    if draw:
+        cv2.line(image, (int(x1),int(y1)),(int(x2),int(y2)), (255, 255, 255), 3 )
+        cv2.line(image, (int(x3),int(y3)),(int(x2),int(y2)), (255, 255, 255), 3 )
+        cv2.line(image, (int(x3),int(y3)),(int(x4),int(y4)), (255, 255, 255), 3 )
+        cv2.line(image, (int(x1),int(y1)),(int(x4),int(y4)), (255, 255, 255), 3 )
+        cv2.circle(image, (int(x1),int(y1)), 10 , (255, 0, 0), cv2.FILLED)
+        cv2.circle(image, (int(x2),int(y2)), 10 , (255, 255, 0), cv2.FILLED)
+        cv2.circle(image, (int(x3),int(y3)), 10 , (255, 255, 255), cv2.FILLED)
+        cv2.circle(image, (int(x4),int(y4)), 10 , (255, 255, 255), cv2.FILLED)
+       
         
--- run_pose.py
+++ run_pose.py
@@ -4,6 +4,9 @@ import torch
 import asyncio
 import argparse
 import numpy as np
+import time
+import datetime
+
 from utils.datasets import letterbox
 from utils.torch_utils import select_device
 from models.experimental import attempt_load
@@ -13,6 +16,7 @@ from torchvision import transforms
 
 from isup import isUpR
 from isup import isUpL
+from isup import Torso
 from nats_client import publish 
 
 @torch.no_grad()
@@ -23,6 +27,13 @@ async def run(poseweights= 'yolov7-w6-pose.pt', source='pose.mp4', device='cpu')
 
     path = source
     ext = path.split('/')[-1].split('.')[-1].strip().lower()
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    
+    detection_stopped_time = None
+    timer_started = False
+    detection = False
+    SECONDS_TO_RECORD_AFTER_DETECTION = 15
+
     if ext in ["mp4", "webm", "avi"] or ext not in ["mp4", "webm", "avi"] and ext.isnumeric():
         input_path = int(path) if path.isnumeric() else path
         device = select_device(opt.device)
@@ -32,25 +43,33 @@ async def run(poseweights= 'yolov7-w6-pose.pt', source='pose.mp4', device='cpu')
 
         cap = cv2.VideoCapture(input_path)
 
+        
+
         if (cap.isOpened() == False):
             print('Error while trying to read video. Please check path again')
-
+        frame_size= (int(cap.get(3)), int(cap.get(4)))
         frame_width, frame_height = int(cap.get(3)), int(cap.get(4))
 
         vid_write_image = letterbox(
             cap.read()[1], (frame_width), stride=64, auto=True)[0]
         resize_height, resize_width = vid_write_image.shape[:2]
         out_video_name = "output" if path.isnumeric else f"{input_path.split('/')[-1].split('.')[0]}"
-        out = cv2.VideoWriter(f"{out_video_name}_result4.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 30, (resize_width, resize_height))
+        #out = cv2.VideoWriter(f"{out_video_name}_result4.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 30, (resize_width, resize_height))
 
         frame_count, total_fps = 0, 0
 
         while cap.isOpened:
-
-            print(f"Frame {frame_count} Processing")
+            
+           
+           # print(f"Frame {frame_count} Processing")
             ret, frame = cap.read()
+            
             if ret:
+                
                 orig_image = frame
+                
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
   
                 # preprocess image
                 image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
@@ -72,15 +91,81 @@ async def run(poseweights= 'yolov7-w6-pose.pt', source='pose.mp4', device='cpu')
                 img = img.cpu().numpy().astype(np.uint8)
 
                 img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+                icon = cv2.imread("botao-x.png")
+                thre = (frame_height//2)+100
+                
+                
 
-            
+                
+
                 for idx in range(output.shape[0]):
                     kpts = output[idx, 7:].T
+                    #plot_skeleton_kpts(img,kpts,3)
+                    xmin, ymin = (output[idx,2] - output[idx,4]/2), (output[idx,3] - output[idx,5]/2)
+                    xmax, ymax = (output[idx,2] + output[idx,4]/2), (output[idx,3] + output[idx,5]/2)
+                    p5 = (int(xmin), int(ymin))
+                    p6 = (int(xmax), int(ymax))
+                    dx = int(xmax) - int(xmin)
+                    dy = int(ymax) - int(ymin)
+                    cx = int(xmin + xmax)//2
+                    cy = int(ymin + ymax)//2
+                    icon = cv2.resize(icon, (50,50), interpolation=cv2.INTER_LINEAR)
+                    difference = dy - dx 
+                    # ph = Get_coord(kpts, 2)
                     Lup = isUpL(img, kpts, 5, 7, 9, draw=True)
                     Rup = isUpR(img, kpts, 6, 8, 10, draw=True)
-
+                    Torso1 = Torso(img, kpts, 6 , 5, 11, 12, draw=True)
+                   # print(difference)
+                   # print(dy)
+                   # print(dx)
                     bothUp: bool = Rup == True and Lup == True
                     
+                    # and (int(ph)> thre))
+                    
+                    if difference < 50:
+                        if detection:
+                            timer_started = False
+                        else:
+                            detection = True
+                            current_time = datetime.datetime.now().strftime("%d-%m-%Y-%H-%S")
+                            out = cv2.VideoWriter(f"{current_time}.mp4", fourcc, 24, frame_size)
+                            print("******************COMECOU A GRAVAR*******************")
+                    elif detection:
+                        if timer_started:
+                            if time.time() - detection_stopped_time >= SECONDS_TO_RECORD_AFTER_DETECTION:
+                                detection = False
+                                timer_started = False
+                                out.release()
+                                print("################# PAROU DE GRAVAR ####################")
+                        else:
+                            timer_started = True
+                            detection_stopped_time = time.time()
+                    
+                    if detection:
+                        out.write(frame)
+                    
+                    if ((difference<0) or (difference < 0)):
+                           # font
+                        font = cv2.FONT_HERSHEY_COMPLEX
+                        
+                        # org
+                        org = (50, 50)
+                        
+                        # fontScale
+                        fontScale = 1
+                        
+                        # Blue color in BGR
+                        color = (0, 0, 0)
+                        
+                        # Line thickness of 2 px
+                        thickness = 2
+                        
+                        cv2.putText(img, 'Caiu vovo', org, font, 
+                                        fontScale, color, thickness, cv2.LINE_AA)
+                        img[cy:cy+50, cx:cx+50] = icon
+                        
+                      
+          
                     if bothUp:
                         # font
                         font = cv2.FONT_HERSHEY_COMPLEX
@@ -177,10 +262,10 @@ async def run(poseweights= 'yolov7-w6-pose.pt', source='pose.mp4', device='cpu')
                 fps = 1 / (end_time - start_time)
                 total_fps += fps
                 frame_count += 1
-                out.write(img)
+                #out.write(img)
             else:
                 break
-
+        out.release()        
         cap.release()
         avg_fps = total_fps / frame_count
         print(f"Average FPS: {avg_fps:.3f}")
